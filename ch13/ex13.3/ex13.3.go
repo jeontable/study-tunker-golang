450575191c35e785ffb3983ef2ed92b13117f27b
jeontable <jeontable@gmail.com>
2026-09-23 11:53:58 +0900

    -

--- ch13/ex13.3/ex13.3.go
+++ ch13/ex13.3/ex13.3.go
@@ -4,8 +4,13 @@ import "fmt"
 
 func main() {
 	var char rune = '한'
+	var char2 int32 = '함'
 
 	fmt.Printf("%T\n", char) //rune == int32(4바이트)
 	fmt.Println(char)
 	fmt.Printf("%c\n", char)
+	fmt.Println()
+	fmt.Printf("%T\n", char2)
+	fmt.Println(char2)
+	fmt.Printf("%c\n", char2)
 }
